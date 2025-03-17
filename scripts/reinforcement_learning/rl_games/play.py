1d5209320a73720bdbb8253cd68dbb81f71f82a1
ricard-inho <ricardmarsalicastan@gmail.com>
2025-03-17 17:39:24 +0100

    play rl_games

--- scripts/reinforcement_learning/rl_games/play.py
+++ scripts/reinforcement_learning/rl_games/play.py
@@ -3,7 +3,7 @@
 #
 # SPDX-License-Identifier: BSD-3-Clause
 
-"""Script to train RL agent with RL-Games."""
+"""Script to play a checkpoint if an RL agent from RL-Games."""
 
 """Launch Isaac Sim Simulator first."""
 
@@ -13,30 +13,30 @@ import sys
 from isaaclab.app import AppLauncher
 
 # add argparse arguments
-parser = argparse.ArgumentParser(description="Train an RL agent with RL-Games.")
+parser = argparse.ArgumentParser(description="Play a checkpoint of an RL agent from RL-Games.")
 parser.add_argument("--video", action="store_true", default=False, help="Record videos during training.")
 parser.add_argument("--video_length", type=int, default=200, help="Length of the recorded video (in steps).")
-parser.add_argument("--video_interval", type=int, default=2000, help="Interval between video recordings (in steps).")
+parser.add_argument(
+    "--disable_fabric", action="store_true", default=False, help="Disable fabric and use USD I/O operations."
+)
 parser.add_argument("--num_envs", type=int, default=None, help="Number of environments to simulate.")
 parser.add_argument("--task", type=str, default=None, help="Name of the task.")
-parser.add_argument("--seed", type=int, default=None, help="Seed used for the environment")
-parser.add_argument(
-    "--distributed", action="store_true", default=False, help="Run training with multiple GPUs or nodes."
-)
 parser.add_argument("--checkpoint", type=str, default=None, help="Path to model checkpoint.")
-parser.add_argument("--sigma", type=str, default=None, help="The policy's initial standard deviation.")
-parser.add_argument("--max_iterations", type=int, default=None, help="RL Policy training iterations.")
-
+parser.add_argument(
+    "--use_last_checkpoint",
+    action="store_true",
+    help="When no checkpoint provided, use the last saved model. Otherwise use the best saved model.",
+)
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
 args_cli, hydra_args = parser.parse_known_args()
+# clear out sys.argv for Hydra
+sys.argv = [sys.argv[0]] + hydra_args
+
 # always enable cameras to record video
 if args_cli.video:
     args_cli.enable_cameras = True
-
-# clear out sys.argv for Hydra
-sys.argv = [sys.argv[0]] + hydra_args
 
 # launch omniverse app
 app_launcher = AppLauncher(args_cli)
@@ -44,14 +44,14 @@ simulation_app = app_launcher.app
 
 """Rest everything follows."""
 
+
 import gymnasium as gym
 import math
 import os
-import random
-from datetime import datetime
+import torch
 
 from rl_games.common import env_configurations, vecenv
-from rl_games.common.algo_observer import IsaacAlgoObserver
+from rl_games.common.player import BasePlayer
 from rl_games.torch_runner import Runner
 
 from isaaclab.envs import (
@@ -63,67 +63,42 @@ from isaaclab.envs import (
 )
 from isaaclab.utils.assets import retrieve_file_path
 from isaaclab.utils.dict import print_dict
-from isaaclab.utils.io import dump_pickle, dump_yaml
-
-from isaaclab_rl.rl_games import RlGamesGpuEnv, RlGamesVecEnvWrapper
 
 import isaaclab_tasks  # noqa: F401
+from isaaclab_tasks.utils import get_checkpoint_path
 from isaaclab_tasks.utils.hydra import hydra_task_config
+from isaaclab_rl.rl_games import RlGamesGpuEnv, RlGamesVecEnvWrapper
 
 
 @hydra_task_config(args_cli.task, "rl_games_cfg_entry_point")
 def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agent_cfg: dict):
-    """Train with RL-Games agent."""
+    # Use `env_cfg` and `agent_cfg` directly where necessary.
     # override configurations with non-hydra CLI arguments
     env_cfg.scene.num_envs = args_cli.num_envs if args_cli.num_envs is not None else env_cfg.scene.num_envs
     env_cfg.sim.device = args_cli.device if args_cli.device is not None else env_cfg.sim.device
 
-    # randomly sample a seed if seed = -1
-    if args_cli.seed == -1:
-        args_cli.seed = random.randint(0, 10000)
-
-    agent_cfg["params"]["seed"] = args_cli.seed if args_cli.seed is not None else agent_cfg["params"]["seed"]
-    agent_cfg["params"]["config"]["max_epochs"] = (
-        args_cli.max_iterations if args_cli.max_iterations is not None else agent_cfg["params"]["config"]["max_epochs"]
-    )
-    if args_cli.checkpoint is not None:
-        resume_path = retrieve_file_path(args_cli.checkpoint)
-        agent_cfg["params"]["load_checkpoint"] = True
-        agent_cfg["params"]["load_path"] = resume_path
-        print(f"[INFO]: Loading model checkpoint from: {agent_cfg['params']['load_path']}")
-    train_sigma = float(args_cli.sigma) if args_cli.sigma is not None else None
-
-    # multi-gpu training config
-    if args_cli.distributed:
-        agent_cfg["params"]["seed"] += app_launcher.global_rank
-        agent_cfg["params"]["config"]["device"] = f"cuda:{app_launcher.local_rank}"
-        agent_cfg["params"]["config"]["device_name"] = f"cuda:{app_launcher.local_rank}"
-        agent_cfg["params"]["config"]["multi_gpu"] = True
-        # update env config device
-        env_cfg.sim.device = f"cuda:{app_launcher.local_rank}"
-
-    # set the environment seed (after multi-gpu config for updated rank from agent seed)
-    # note: certain randomizations occur in the environment initialization so we set the seed here
-    env_cfg.seed = agent_cfg["params"]["seed"]
-
+    """Play with RL-Games agent."""
     # specify directory for logging experiments
-    log_root_path = os.path.join("logs", "rl_games", agent_cfg["params"]["config"]["name"])
+    log_root_path = os.path.join("logs", "rl_games", agent_cfg["params"]["config"]["name"], args_cli.task.split("-")[2])
     log_root_path = os.path.abspath(log_root_path)
-    print(f"[INFO] Logging experiment in directory: {log_root_path}")
-    # specify directory for logging runs
-    log_dir = agent_cfg["params"]["config"].get("full_experiment_name", datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-    # set directory into agent config
-    # logging directory path: <train_dir>/<full_experiment_name>
-    agent_cfg["params"]["config"]["train_dir"] = log_root_path
-    agent_cfg["params"]["config"]["full_experiment_name"] = log_dir
+    print(f"[INFO] Loading experiment from directory: {log_root_path}")
+    # find checkpoint
+    if args_cli.checkpoint is None:
+        # specify directory for logging runs
+        run_dir = agent_cfg["params"]["config"].get("full_experiment_name", ".*")
+        # specify name of checkpoint
+        if args_cli.use_last_checkpoint:
+            checkpoint_file = ".*"
+        else:
+            # this loads the best checkpoint
+            checkpoint_file = f"{agent_cfg['params']['config']['name']}.pth"
+        # get path to previous checkpoint
+        resume_path = get_checkpoint_path(log_root_path, run_dir, checkpoint_file, other_dirs=["nn"])
+    else:
+        resume_path = retrieve_file_path(args_cli.checkpoint)
+    log_dir = os.path.dirname(os.path.dirname(resume_path))
 
-    # dump the configuration into log-directory
-    dump_yaml(os.path.join(log_root_path, log_dir, "params", "env.yaml"), env_cfg)
-    dump_yaml(os.path.join(log_root_path, log_dir, "params", "agent.yaml"), agent_cfg)
-    dump_pickle(os.path.join(log_root_path, log_dir, "params", "env.pkl"), env_cfg)
-    dump_pickle(os.path.join(log_root_path, log_dir, "params", "agent.pkl"), agent_cfg)
-
-    # read configurations about the agent-training
+    # wrap around environment for rl-games
     rl_device = agent_cfg["params"]["config"]["device"]
     clip_obs = agent_cfg["params"]["env"].get("clip_observations", math.inf)
     clip_actions = agent_cfg["params"]["env"].get("clip_actions", math.inf)
@@ -138,8 +113,8 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     # wrap for video recording
     if args_cli.video:
         video_kwargs = {
-            "video_folder": os.path.join(log_root_path, log_dir, "videos", "train"),
-            "step_trigger": lambda step: step % args_cli.video_interval == 0,
+            "video_folder": os.path.join(log_root_path, log_dir, "videos", "play"),
+            "step_trigger": lambda step: step == 0,
             "video_length": args_cli.video_length,
             "disable_logger": True,
         }
@@ -157,19 +132,56 @@ def main(env_cfg: ManagerBasedRLEnvCfg | DirectRLEnvCfg | DirectMARLEnvCfg, agen
     )
     env_configurations.register("rlgpu", {"vecenv_type": "IsaacRlgWrapper", "env_creator": lambda **kwargs: env})
 
+    # load previously trained model
+    agent_cfg["params"]["load_checkpoint"] = True
+    agent_cfg["params"]["load_path"] = resume_path
+    print(f"[INFO]: Loading model checkpoint from: {agent_cfg['params']['load_path']}")
+
     # set number of actors into agent config
     agent_cfg["params"]["config"]["num_actors"] = env.unwrapped.num_envs
     # create runner from rl-games
-    runner = Runner(IsaacAlgoObserver())
+    runner = Runner()
     runner.load(agent_cfg)
+    # obtain the agent from the runner
+    agent: BasePlayer = runner.create_player()
+    agent.restore(resume_path)
+    agent.reset()
 
-    # reset the agent and env
-    runner.reset()
-    # train the agent
-    if args_cli.checkpoint is not None:
-        runner.run({"train": True, "play": False, "sigma": train_sigma, "checkpoint": resume_path})
-    else:
-        runner.run({"train": True, "play": False, "sigma": train_sigma})
+    # reset environment
+    obs = env.reset()
+    if isinstance(obs, dict):
+        obs = obs["obs"]
+    timestep = 0
+    # required: enables the flag for batched observations
+    _ = agent.get_batch_size(obs, 1)
+    # initialize RNN states if used
+    if agent.is_rnn:
+        agent.init_rnn()
+    # simulate environment
+    # note: We simplified the logic in rl-games player.py (:func:`BasePlayer.run()`) function in an
+    #   attempt to have complete control over environment stepping. However, this removes other
+    #   operations such as masking that is used for multi-agent learning by RL-Games.
+    while simulation_app.is_running():
+        # run everything in inference mode
+        with torch.inference_mode():
+            # convert obs to agent format
+            obs = agent.obs_to_torch(obs)
+            # agent stepping
+            actions = agent.get_action(obs, is_deterministic=agent.is_deterministic)
+            # env stepping
+            obs, _, dones, _ = env.step(actions)
+
+            # perform operations for terminated episodes
+            if len(dones) > 0:
+                # reset rnn state for terminated episodes
+                if agent.is_rnn and agent.states is not None:
+                    for s in agent.states:
+                        s[:, dones, :] = 0.0
+        if args_cli.video:
+            timestep += 1
+            # Exit the play loop after recording one video
+            if timestep == args_cli.video_length:
+                break
 
     # close the simulator
     env.close()
